8cbf4d21885da6fd78247d58935e9b7b6c98f96d
Huy Tran Tuan <trantuanhuyltv@gmail.com>
2025-05-12 18:20:10 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import path from "path";
+export default defineConfig({
+    root: "src",
+    publicDir: "../public",
+    build: {
+        outDir: "../dist",
+        emptyOutDir: true,
+        rollupOptions:{
+            input: {
+                index: path.resolve(__dirname, "src/index.html"),
+                collection: path.resolve(__dirname, "src/collection.html"),
+                account: path.resolve(__dirname, "src/account.html"),
+                payment: path.resolve(__dirname, "src/payment.html"),
+                signin: path.resolve(__dirname, "src/sign-in.html"),
+                signup: path.resolve(__dirname, "src/sign-up.html"),
+            }
+        }
+    },
+    plugins: [ViteEjsPlugin()],
+})
